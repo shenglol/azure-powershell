438ef324493a165ed52ef4f5cd015b2bc854d845
Shenglong Li <shenglol@microsoft.com>
2019-10-30 14:55:43 -0700

    noises => noise.

--- src/Resources/Resources.Test/Formatters/WhatIfOperationResultFormatterTests.cs
+++ src/Resources/Resources.Test/Formatters/WhatIfOperationResultFormatterTests.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Azure.Commands.Resources.Test.Formatters
             // Arrange.
             var psWhatIfOperationResult = new PSWhatIfOperationResult(new WhatIfOperationResult());
             string previewNotice = new ColorStringBuilder()
-                .AppendLine("Note: As What-If is currently in preview, the result may contain false positive predictions (noises).")
+                .AppendLine("Note: As What-If is currently in preview, the result may contain false positive predictions (noise).")
                 .AppendLine("You can help us improve the accuracy of the result by opening an issue here: https://aka.ms/WhatIfIssues.")
                 .ToString();
 
